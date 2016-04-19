d920aa2d0f1f350b6c18ecd6f6996796fbd8700b
Lei Zhang <LeiZhang@dyn-118-139-34-240.its.monash.edu.au>
2016-04-19 23:45:34 +1000

    change index as theme

--- Login-Signup-PDO-OOP/index.php
+++ Login-Signup-PDO-OOP/index.php
@@ -44,11 +44,45 @@ if(isset($_POST['btn-login']))
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Active Family : Login</title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/custom.css"/>
 <link rel="stylesheet" href="style.css" type="text/css"  />
 </head>
 <body>
+<!-- ******HEADER****** -->
+<header id="header" class="header navbar-fixed-top" style="position: relative;">
+    <div class="container">
+        <h1 class="logo">
+            <a href="http://active-family.net"><span class="logo-icon"></span><span class="text">Active Family</span></a>
+        </h1><!--logo-->
+        <nav class="main-nav navbar-right" role="navigation">
+            <div class="navbar-header">
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button><!--nav-toggle-->
+            </div><!--navbar-header-->
+            <div id="navbar-collapse" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item"><a href="http://active-family.net/">Home</a></li>
+                    <li class="active nav-item"><a href="http://active-family.net/map/">Venues</a></li>
+                    <li class="nav-item"><a href="http://active-family.net/about.html">About Us</a></li>
+                    <li class="nav-item"><a href="http://localhost:8888/active%20family/Login-Signup-PDO-OOP/index.php">Log in</a></li>
+                    <li class="nav-item nav-item-cta last"><a class="btn btn-cta btn-cta-secondary" href="#">Sign Up Free</a></li>
+                </ul><!--nav-->
+            </div><!--navabr-collapse-->
+        </nav><!--main-nav-->
+    </div><!--container-->
+</header><!--header-->
 
 <div class="signin-form">
 
