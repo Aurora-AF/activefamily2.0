2d2a8a4982b6a33b14899c4841dd20c668f76115
Lei Zhang <LeiZhang@dyn-118-139-53-167.its.monash.edu.au>
2016-04-20 20:00:46 +1000

    index layout

--- Login-Signup-PDO-OOP/index.php
+++ Login-Signup-PDO-OOP/index.php
@@ -5,17 +5,17 @@ $login = new USER();
 
 if($login->is_loggedin()!="")
 {
-	$login->redirect('home.php');
+    $login->redirect('home.php');
 }
 
 if(isset($_POST['btn-login']))
 {
-	$uname = strip_tags($_POST['txt_uname_email']);
-	$umail = strip_tags($_POST['txt_uname_email']);
-	$upass = strip_tags($_POST['txt_password']);
+    $uname = strip_tags($_POST['txt_uname_email']);
+    $umail = strip_tags($_POST['txt_uname_email']);
+    $upass = strip_tags($_POST['txt_password']);
 
-	if($login->doLogin($uname,$umail,$upass))
-	{
+    if($login->doLogin($uname,$umail,$upass))
+    {
         if(isset($_SESSION['url'])) {
             $url = $_SESSION['url']; // holds url for last page visited.
             $login->redirect($url);
@@ -32,66 +32,103 @@ if(isset($_POST['btn-login']))
 
 
         //header("Location:$url");
-	}
-	else
-	{
-		$error = "Wrong Details !";
-	}
+    }
+    else
+    {
+        $error = "Wrong Details !";
+    }
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Active Family : Login</title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-<link rel="stylesheet" href="style.css" type="text/css"  />
-
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Active Family : Login</title>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/custom.css"/>
+    <link rel="stylesheet" href="style.css" type="text/css"  />
 </head>
-<body>
+<body style="background-color: #f5f5f5">
+<!-- ******HEADER****** -->
+<header id="header" class="header navbar-fixed-top" style="position: relative;">
+    <div class="container">
+        <h1 class="logo">
+            <a href="http://active-family.net"><span class="logo-icon"></span><span class="text">Active Family</span></a>
+        </h1><!--logo-->
+        <nav class="main-nav navbar-right" role="navigation">
+            <div class="navbar-header">
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button><!--nav-toggle-->
+            </div><!--navbar-header-->
+            <div id="navbar-collapse" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item"><a href="http://active-family.net/">Home</a></li>
+                    <li class="active nav-item"><a href="http://active-family.net/map/">Venues</a></li>
+                    <li class="nav-item"><a href="http://active-family.net/about.html">About Us</a></li>
+                    <li class="nav-item"><a href="http://localhost:8888/active%20family/Login-Signup-PDO-OOP/index.php">Log in</a></li>
+                    <li class="nav-item nav-item-cta last"><a class="btn btn-cta btn-cta-secondary" href="#">Sign Up Free</a></li>
+                </ul><!--nav-->
+            </div><!--navabr-collapse-->
+        </nav><!--main-nav-->
+    </div><!--container-->
+</header><!--header-->
 
 <div class="signin-form">
 
-	<div class="container">
+    <div class="container" >
 
 
-       <form class="form-signin" method="post" id="login-form">
+        <form class="form-signin" method="post" id="login-form" style="background-color: #f5f5f5">
 
-        <h2 class="form-signin-heading">Log In to Active Family.</h2><hr />
+            <h2 class="title" style="font-size: 30px">Log In to Active Family.</h2><hr />
 
-        <div id="error">
-        <?php
-			if(isset($error))
-			{
-				?>
-                <div class="alert alert-danger">
-                   <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
-                </div>
+            <div id="error">
                 <?php
-			}
-		?>
-        </div>
+                if(isset($error))
+                {
+                    ?>
+                    <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
 
-        <div class="form-group">
-        <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
-        <span id="check-e"></span>
-        </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+                <span id="check-e"></span>
+            </div>
 
-        <div class="form-group">
-        <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
-        </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
+            </div>
 
-     	<hr />
+            <div class="form-group">
 
-        <div class="form-group">
-            <button type="submit" name="btn-login" class="btn btn-default">
-                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; LOG IN
-            </button>
-        </div>
-      	<br />
-            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
-            <label>Forget Your Password by Your Email! <a href="resetPage.php">Reset</a></label>
+            </div>
+
+            <hr />
+
+            <div class="form-group">
+                <button type="submit" name="btn-login" class="btn btn-primary btn-lg">
+                    <i class="glyphicon glyphicon-log-in"></i> &nbsp; LOG IN
+                </button>
+            </div>
+            <br />
+            <label style="color: #ffa400">Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
+            <label style="color: #ffa400" >Forget Your Password by Your Email ! <a href="resetPage.php">Reset</a></label>
       </form>
 
     </div>
